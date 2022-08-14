3754393a862c5934b8dc1f0fc7f9f3a058261681
shota-aa <dqx1005@icloud.com>
2022-08-14 16:41:46 +0900

    hard cord

--- go/main.go
+++ go/main.go
@@ -145,10 +145,10 @@ func (r *RecordMapper) Err() error {
 }
 
 func NewMySQLConnectionEnv() *MySQLConnectionEnv {
-	fmt.Println(getEnv("MYSQL_HOST", "10.0.0.102"))
+	// fmt.Println(getEnv("MYSQL_HOST", "10.0.0.102"))
 	return &MySQLConnectionEnv{
 		// Host:     getEnv("MYSQL_HOST", "127.0.0.1"),
-		Host:     getEnv("MYSQL_HOST", "10.0.0.102"),
+		Host:     "10.0.0.102",
 		Port:     getEnv("MYSQL_PORT", "3306"),
 		User:     getEnv("MYSQL_USER", "isucon"),
 		DBName:   getEnv("MYSQL_DBNAME", "isuumo"),
