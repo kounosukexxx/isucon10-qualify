cb7b3f9a7e31c26bd664ac655044c509bd8ce8a5
shota-aa <dqx1005@icloud.com>
2022-08-14 16:25:41 +0900

    fix mysql host

--- go/main.go
+++ go/main.go
@@ -146,7 +146,8 @@ func (r *RecordMapper) Err() error {
 
 func NewMySQLConnectionEnv() *MySQLConnectionEnv {
 	return &MySQLConnectionEnv{
-		Host:     getEnv("MYSQL_HOST", "127.0.0.1"),
+		// Host:     getEnv("MYSQL_HOST", "127.0.0.1"),
+		Host:     getEnv("MYSQL_HOST", "10.0.0.102"),
 		Port:     getEnv("MYSQL_PORT", "3306"),
 		User:     getEnv("MYSQL_USER", "isucon"),
 		DBName:   getEnv("MYSQL_DBNAME", "isuumo"),
